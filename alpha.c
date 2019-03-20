dcef002ac852acc0d8f8d919822bd426093fb440
logeshlogesh <48052938+logeshlogesh@users.noreply.github.com>
2019-03-20 16:15:42 +0530

    Create alpha.c

---
+++ alpha.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+char x;
+printf("enter the character:");
+scanf("%c",&c);
+if((x>='a' && x<='z') || (x>='A' && x<='Z'))
+{
+printf("given character is alphabet");
+}
+else
+{
+printf("not alphabet");
+}
+}
